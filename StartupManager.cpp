27d8669016717fa9bb1616c157718b7fb38fee00
OscarwOw <OscarwOw210@gmail.com>
2024-10-16 00:21:28 +0200

    Add project files.

---
+++ StartupManager.cpp
@@ -0,0 +1,77 @@
+#include "StartupManager.hpp"
+#include <iostream>
+
+void StartupManager::InitializeGLFW() {
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR: could not start GLFW3\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int StartupManager::InitializeGLEW(GLFWwindow* window) {
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "ERROR: could not initialize GLEW\n");
+        return -1;
+    }
+    return 0;
+}
+
+void StartupManager::SetWindowHints() {
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* 
+        //OpenGL Legacy allowed
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+        //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE); //for any profile
+    */
+}
+
+GLFWwindow* StartupManager::CreateWindow(int width, int height, const char* title) {
+    GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        std::cerr << "ERROR: Could not create GLFW window\n";
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    return window;
+}
+
+void StartupManager::PrintInfo() {
+    printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
+    printf("Using GLEW %s\n", glewGetString(GLEW_VERSION));
+    printf("Vendor %s\n", glGetString(GL_VENDOR));
+    printf("Renderer %s\n", glGetString(GL_RENDERER));
+    printf("GLSL %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+    int major, minor, revision;
+    glfwGetVersion(&major, &minor, &revision);
+    printf("Using GLFW %i.%i.%i\n", major, minor, revision);
+}
+
+void StartupManager::ViewPortSetup(GLFWwindow* window) {
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    float ratio = width / (float)height;
+    glViewport(0, 0, width, height);
+}
+
+
+
+void StartupManager::DrawLegacyTriangles(){
+    glBegin(GL_TRIANGLES);  // Start drawing triangles
+    glColor3f(1.0f, 0.0f, 0.0f);  // Red color
+    glVertex3f(-0.5f, -0.5f, 0.0f);  // First vertex (bottom-left)
+
+    glColor3f(0.0f, 1.0f, 0.0f);  // Green color
+    glVertex3f(0.5f, -0.5f, 0.0f);  // Second vertex (bottom-right)
+
+    glColor3f(0.0f, 0.0f, 1.0f);  // Blue color
+    glVertex3f(0.0f, 0.5f, 0.0f);  // Third vertex (top)
+    glEnd();  // Finish drawing triangles
+}
---
+++ ZPGProj.cpp
@@ -0,0 +1,149 @@
+﻿#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>  
+
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "StartupManager.hpp"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+static void error_callback(int error, const char* description) { fputs(description, stderr); }
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	printf("key_callback [%d,%d,%d,%d] \n", key, scancode, action, mods);
+}
+
+static void window_focus_callback(GLFWwindow* window, int focused) { printf("window_focus_callback \n"); }
+
+static void window_iconify_callback(GLFWwindow* window, int iconified) { printf("window_iconify_callback \n"); }
+
+static void window_size_callback(GLFWwindow* window, int width, int height) {
+	printf("resize %d, %d \n", width, height);
+	glViewport(0, 0, width, height);
+}
+
+static void cursor_callback(GLFWwindow* window, double x, double y) { printf("cursor_callback \n"); }
+
+static void button_callback(GLFWwindow* window, int button, int action, int mode) {
+	if (action == GLFW_PRESS) printf("button_callback [%d,%d,%d]\n", button, action, mode);
+}
+
+static void cursor_pos_callback(GLFWwindow* window, double mouseX, double mouseY) {
+    printf("cursor_pos_callback %d, %d;\n", (int)mouseX, (int)mouseY);
+}
+
+
+float points[] = {
+	 0.0f, 0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	-0.5f, -0.5f, 0.0f
+};
+
+
+const char* vertex_shader =
+"#version 330\n"
+"layout(location=0) in vec3 vp;"
+"void main () {"
+"     gl_Position = vec4 (vp, 1.0);"
+"}";
+
+
+const char* fragment_shader =
+"#version 330\n"
+"out vec4 frag_colour;"
+"void main () {"
+"     frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
+"}";
+
+
+
+int main(void)
+{
+    glfwSetErrorCallback(error_callback);
+
+    StartupManager::InitializeGLFW();
+    StartupManager::SetWindowHints();
+    GLFWwindow* window = StartupManager::CreateWindow(800, 600, "My OpenGL Window");
+
+    if (StartupManager::InitializeGLEW(window) == -1)  return -1;
+
+    StartupManager::PrintInfo();
+
+    StartupManager::ViewPortSetup(window);
+
+    glfwSetCursorPosCallback(window, [](GLFWwindow* window, double mouseXPos, double mouseYPos)-> void {cursor_pos_callback(window, mouseXPos, mouseYPos); });
+
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+     
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, NULL);
+
+    glBindVertexArray(0);
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertex_shader, NULL);
+    glCompileShader(vertexShader);
+    
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragment_shader, NULL);
+    glCompileShader(fragmentShader);
+    
+    
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, fragmentShader);
+    glAttachShader(shaderProgram, vertexShader);
+    glLinkProgram(shaderProgram);
+
+
+    GLint status;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
+    if (status == GL_FALSE)
+    {
+        GLint infoLogLength;
+        glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &infoLogLength);
+        GLchar* strInfoLog = new GLchar[infoLogLength + 1];
+        glGetProgramInfoLog(shaderProgram, infoLogLength, NULL, strInfoLog);
+        fprintf(stderr, "Linker failure: %s\n", strInfoLog);
+        delete[] strInfoLog;
+    }
+
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+
+        glBindVertexArray(VAO);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3); 
+
+        
+
+        glfwPollEvents();
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
