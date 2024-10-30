9f31519d97afd37e5586fd04ec9430f9e985c7dc
Adam Belos <bel0130@vsb.cz>
2024-10-30 08:49:35 +0100

    camera repair

--- MatrixHelper.cpp
+++ MatrixHelper.cpp
@@ -5,7 +5,7 @@
 void MatrixHelper::printMatrix(const glm::mat4& matrix) {
     for (int i = 0; i < 4; ++i) {
         for (int j = 0; j < 4; ++j) {
-            std::cout << matrix[j][i] << "\t"; 
+            std::cout << matrix[j][i] << "\t\t\t"; 
         }
         std::cout << std::endl;
     }
